759628595964df1ec459e98e595b235386f5d847
Cinka Sihaloho <cinkasihaloho91@gmail.com>
2020-11-19 19:35:58 +0700

    pertemuan4

--- app/src/main/java/com/example/cerc_materialdesign/Adapter/GridHeroAdapter.java
+++ app/src/main/java/com/example/cerc_materialdesign/Adapter/GridHeroAdapter.java
@@ -1,14 +1,19 @@
 package com.example.cerc_materialdesign.Adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.cerc_materialdesign.Activity.DetailActivity;
 import com.example.cerc_materialdesign.Model.Hero;
 import com.example.cerc_materialdesign.R;
 
@@ -16,9 +21,11 @@ import java.util.ArrayList;
 
 public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridViewHolder> {
     private ArrayList<Hero> listHero;
+    private Context context;
 
-    public GridHeroAdapter(ArrayList<Hero> list) {
+    public GridHeroAdapter(ArrayList<Hero> list,Context context) {
         this.listHero = list;
+        this.context=context;
     }
     private ListHeroAdapter.OnItemClickCallback onItemClickCallback;
     public void setOnItemClickCallback(ListHeroAdapter.OnItemClickCallback onItemClickCallback) {
@@ -37,6 +44,19 @@ public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridVi
                 .load(listHero.get(position).getPhoto())
                 .apply(new RequestOptions().override(350, 550))
                 .into(holder.imgPhoto);
+
+        final Hero hero = listHero.get(position);
+        holder.imgPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context, DetailActivity.class);
+                intent.putExtra("nama",hero.getName());
+                intent.putExtra("from",hero.getFrom());
+                intent.putExtra("foto",hero.getPhoto());
+                context.startActivity(intent);
+                Toast.makeText(holder.itemView.getContext(), "Kamu memilih " + listHero.get(holder.getAdapterPosition()).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -49,7 +69,7 @@ public class GridHeroAdapter extends RecyclerView.Adapter<GridHeroAdapter.GridVi
 
         GridViewHolder(View itemView) {
             super(itemView);
-            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            imgPhoto = itemView.findViewById(R.id.image);
         }
     }
 }
